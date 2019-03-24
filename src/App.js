9ffbf69e01581d3aee69fc4f9e70733879831ba7
Terr1bus <qwant94@mail.ru>
2019-03-24 22:21:18 +1000

    feat: add functional, styles, credentials, axios instance etc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Authorization from './components/AuthorizationView.jsx';
-import GithubStatView from './components/GithubStatView.jsx'
+import Authorization from './modules/auth/AuthorizationView';
+import GithubStatView from './modules/stat/GithubStatView'
+import ProfileView from './modules/profile/ProfileView';
+import SearchView from './modules/search/SearchView';
+import HeaderComponent from './modules/core/components/HeaderComponent';
+import FooterComponent from './modules/core/components/FooterComponent';
+import ErrorBoundary from './modules/core/components/ErrorBoundary';
 import './App.css';
+
+require('dotenv').config();
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Authorization />
-        </div>
+      <div className="App">
+        <ErrorBoundary>
+          <Router>
+            <HeaderComponent />
+              <div className='container'>
+                <div className='main'>
+                  <Route exact path='/' component={ Authorization } />
+                  <Route path='/stat' component={ GithubStatView } />
+                  <Route path='/users/:login' component={ ProfileView } />
+                  <Route path='/search' component={ SearchView } />
+                </div>
+              </div>
+            </Router>
+          </ErrorBoundary>
+          
+        <FooterComponent />
+      </div>
 
-        <Route exact path='/' component={ Authorization } />
-        <Route path='/stat' component={ GithubStatView } />
         
-      </Router>
     );
   }
 }
